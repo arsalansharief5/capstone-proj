8f3b003e1b6b71171368ce5ac53f60556c26abe7
Gov-10 <govindsys1008@gmail.com>
2025-11-14 11:12:29 +0000

    added a protected test endpoint

--- django_back/backapp/api.py
+++ django_back/backapp/api.py
@@ -1,10 +1,11 @@
 from ninja import NinjaAPI
 from .auth import CustomAuth
+from .schema import HelloTestResponse
 api = NinjaAPI()
 @api.get("/hello")
 def hello(request):
     return {"hello": "message"}
 
 @api.get("/secure-hello", auth=CustomAuth())
-def secure_hello(request):
-    return {"secure_hello": "This is a secure message"}
+def secure_hello(request, payload: HelloTestResponse):
+    return {"secure_hello": payload.text}
---
+++ django_back/backapp/schema.py
@@ -0,0 +1,3 @@
+from ninja import Schema
+class HelloTestResponse(Schema):
+    text : str
