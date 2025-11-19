4371a08c6091751810733a88bab6f0df5bd499b5
Govind <govindsys1008@gmail.com>
2025-11-19 13:27:52 +0530

    switched to neon

--- fastapi_agents/database.py
+++ fastapi_agents/database.py
@@ -3,6 +3,9 @@ from fastapi import Query, Depends
 from datetime import datetime
 from typing import Annotated, List, Optional
 import uuid
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class User(SQLModel, table=True):
     email: str = Field(primary_key=True)
     created_at : datetime = Field(default_factory=datetime.utcnow)
@@ -15,10 +18,8 @@ class ChatHistory(SQLModel, table=True):
     user_email: str = Field(foreign_key="user.email")
     user: Optional[User] = Relationship(back_populates="chat_history")
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+NEON_URL = os.getenv("NEON_URL")
+engine = create_engine(NEON_URL, echo=True)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 def get_session():
